7baf3de4155e4effee227c1abae82501cbc421d7
Chillyo <nftyieldfarms@gmail.com>
2021-07-05 18:15:15 +0700

    Color Update

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2883,15 +2883,15 @@ var baseColors = {
     primaryDark: "#aa8929",
     secondary: "#fcca3f",
     success: "#fcca3f",
-    warning: "#FFB237",
+    warning: "#fcca3f",
 };
 var brandColors = {
     binance: "#F0B90B",
 };
-var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#FAF9FA", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#FFB237", textDisabled: "#BDC2C4", textSubtle: "#FFB237", borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
+var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#FAF9FA", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#fcca3f", textDisabled: "#BDC2C4", textSubtle: "#fcca3f", borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
     } });
-var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#FFB237", background: "#343135", backgroundDisabled: "#3c3742", contrast: "#343135", invertedContrast: "#191326", input: "#000000", primaryDark: "#0098A1", tertiary: "#343135", text: "#FFFFFF", textDisabled: "#666171", textSubtle: "#FFB237", borderColor: "#524B63", card: "#27262c", gradients: {
+var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#fcca3f", background: "#343135", backgroundDisabled: "#3c3742", contrast: "#343135", invertedContrast: "#191326", input: "#000000", primaryDark: "#0098A1", tertiary: "#343135", text: "#FFFFFF", textDisabled: "#666171", textSubtle: "#fcca3f", borderColor: "#524B63", card: "#27262c", gradients: {
         bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
     } });
 
